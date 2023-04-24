fb2d84e09a83b32cd5fd2ca5763c57c08bfcf9eb
RammarxR <131293668+RammarxR@users.noreply.github.com>
2023-04-24 10:01:08 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,3 @@
-def checkout(String url, String branch = 'master') {
-  // Perform the checkout using the provided URL and branch
-  git url: url, branch: branch
+def test(name){
+  echo "this is ${name}"
 }
